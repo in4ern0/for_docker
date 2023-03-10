3c0fa6712c7c4a42090017cfce5dad0a5fa194ed
Ilkin <ilkin.mammadovv@gmail.com>
2023-03-10 02:25:45 -0500

    Update centos.sh

--- centos.sh
+++ centos.sh
@@ -4,7 +4,7 @@ sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
-sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo systemctl enable --now docker
 
