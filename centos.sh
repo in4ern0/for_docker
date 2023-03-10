23c9736afa713bb5dc763bb9ee98428d64db51a0
Ilkin <ilkin.mammadovv@gmail.com>
2023-03-10 02:06:02 -0500

    Update centos.sh

--- centos.sh
+++ centos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-selinux docker-engine-selinux docker-engine
+sudo yum remove  -y docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-selinux docker-engine-selinux docker-engine
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
